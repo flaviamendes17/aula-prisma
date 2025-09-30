f895af0cd5aaf0b1d98838b4be9f2a559422fa6b
Flavia Mendes <flavinharamendes@gmail.com>
2025-09-30 09:49:43 -0300

    cria o movie routes completo

--- src/routes/movies.routes.js
+++ src/routes/movies.routes.js
@@ -1,5 +1,9 @@
 import express from "express";
+import movieController from "../controllers/movies.controller.js";
 
 const movieRouter = express.Router();
+
+movieRouter.get("/", movieController.findAll);
+movieRouter.post("", movieController.create);
 
 export default movieRouter;
